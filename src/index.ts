387cff15e8befce00db43ba3ce4b0334881dcc43
marnixah <marnix@hagehoeve.com>
2022-01-24 19:58:42 +0100

    feat: intial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import WebSocket, { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (ws: WebSocket) => {
+  ws.on("message", (message: string) => {
+    console.log(`Received message => ${message}`);
+  });
+  ws.send("Hello World");
+});
